e04cc4f8756ccece2a63209ebbbbfc50641cc623
Simon Berg <ksb@fluffware.se>
2012-02-10 20:02:48 +0100

    Added Debian package support

---
+++ subrec/about_dialog.h
@@ -0,0 +1,7 @@
+#ifndef __ABOUT_DIALOG_H__638CQSV9IQ__
+#define __ABOUT_DIALOG_H__638CQSV9IQ__
+#include <gtk/gtk.h>
+void
+show_about_dialog(GtkWidget *parent);
+
+#endif /* __ABOUT_DIALOG_H__638CQSV9IQ__ */
--- subrec/builderutils.c
+++ subrec/builderutils.c
@@ -1,12 +1,12 @@
 #include "builderutils.h"
-
+#include "dir_config.h"
 gboolean
 builderutils_add_from_file(GtkBuilder *builder, gchar *filename, GError **err)
 {
   gboolean res;
   gchar *path;
   const gchar *uidir = g_getenv("UIDIR");
-  if (!uidir) uidir = G_STRINGIFY(UIDIR);
+  if (!uidir) uidir = UIDIR;
   path= g_build_filename(uidir, filename, NULL);
   res = gtk_builder_add_from_file(builder, path, err);
   g_free(path);
